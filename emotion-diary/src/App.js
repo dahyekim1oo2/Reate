b271404ace8ea94b67627a23250098743389169d
dahye <ekgp14789@naver.com>
2023-12-08 14:30:47 +0900

    commit

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h2>App.js</h2>
+        <h2>App.jsaa</h2>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
